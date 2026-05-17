0930dd05919318df3b63f4b9db5c128ddba234bd
VTrelat <vincent.trelat@depinfonancy.net>
2026-05-17 06:05:48 +0200

    feat: prove add, mul, eq cases of simplifier_partial_correct'
    Discharge the absorbing arithmetic/equality operators via per-operator
    `key` sub-lemmas that peel `simplifier_aux_*` into a denotation
    disjunction.

--- B/Reasoning/SimplifierCorrect/Lemmas.lean
+++ B/Reasoning/SimplifierCorrect/Lemmas.lean
@@ -72,8 +72,178 @@ theorem simplifier_partial_correct' {t : Term} {«Δ»}
     rw [Option.bind_eq_none_iff] at h
     specialize h _ den_simpy.symm
     nomatch h
-  | add x y x_ih y_ih => sorry
-  | mul x y x_ih y_ih => sorry
+  | add x y x_ih y_ih =>
+    obtain ⟨rfl, typx, typy⟩ := Typing.addE typ_t
+    -- peel `⟦a +ᴮ b⟧ = none` into a disjunction
+    have peel_add : ∀ (a b : Term), Γ ⊢ᴮ a : .int → Γ ⊢ᴮ b : .int →
+        ∀ (hab : ∀ v ∈ fv (a +ᴮ b), («Δ» v).isSome = true)
+          (ha : ∀ v ∈ fv a, («Δ» v).isSome = true)
+          (hb : ∀ v ∈ fv b, («Δ» v).isSome = true),
+        ⟦(a +ᴮ b).abstract «Δ» hab⟧ᴮ = none →
+        ⟦a.abstract «Δ» ha⟧ᴮ = none ∨ ⟦b.abstract «Δ» hb⟧ᴮ = none := by
+      intro a b typa typb hab ha hb hnone
+      by_cases ha' : ⟦a.abstract «Δ» ha⟧ᴮ = none
+      · exact Or.inl ha'
+      · obtain ⟨⟨A, _, hA⟩, den_a⟩ := Option.ne_none_iff_exists'.mp ha'
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .int,
+          Typing.of_abstract ha typa⟩ den_a
+        right
+        rw [Option.eq_none_iff_forall_ne_some]
+        rintro ⟨B, _, hB⟩ den_b
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .int,
+          Typing.of_abstract hb typb⟩ den_b
+        simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+          Option.bind_eq_none_iff] at hnone
+        specialize hnone ⟨A, .int, hA⟩ den_a
+        rw [Option.bind_eq_none_iff] at hnone
+        exact absurd (hnone ⟨B, .int, hB⟩ den_b) (by simp)
+    -- `⟦a⟧ = none → ⟦a +ᴮ b⟧ = none`
+    have add_none_l : ∀ (a b : Term)
+        (hab : ∀ v ∈ fv (a +ᴮ b), («Δ» v).isSome = true)
+        (ha : ∀ v ∈ fv a, («Δ» v).isSome = true),
+        ⟦a.abstract «Δ» ha⟧ᴮ = none → ⟦(a +ᴮ b).abstract «Δ» hab⟧ᴮ = none := by
+      intro a b hab ha hnone
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind, hnone,
+        Option.bind_none]
+    -- self-contained fact about `simplifier_aux_add`
+    have key : ∀ (p q : Term), Γ ⊢ᴮ p : .int → Γ ⊢ᴮ q : .int →
+        ∀ (hpq : ∀ v ∈ fv (simplifier_aux_add p q), («Δ» v).isSome = true)
+          (hp : ∀ v ∈ fv p, («Δ» v).isSome = true)
+          (hq : ∀ v ∈ fv q, («Δ» v).isSome = true),
+        ⟦(simplifier_aux_add p q).abstract «Δ» hpq⟧ᴮ = none →
+        ⟦p.abstract «Δ» hp⟧ᴮ = none ∨ ⟦q.abstract «Δ» hq⟧ᴮ = none := by
+      intro p q typp typq hpq hp hq hnone
+      unfold simplifier_aux_add at hnone
+      split at hnone
+      · exact Or.inr hnone
+      · exact Or.inl hnone
+      · simp_rw [Term.abstract, denote] at hnone; nomatch hnone
+      · -- p = x' +ᴮ int a, q = int b ; result x' +ᴮ int (a+b)
+        left
+        obtain ⟨_, typx', _⟩ := Typing.addE typp
+        refine add_none_l _ _ hp
+          (fun v hv => hp v (by rw [fv, List.mem_append]; left; exact hv)) ?_
+        rcases peel_add _ _ typx' Typing.int _
+          (fun v hv => hp v (by rw [fv, List.mem_append]; left; exact hv))
+          (fun v hv => by rw [fv] at hv; nomatch hv) hnone with hx'_n | hint_n
+        · exact hx'_n
+        · simp_rw [Term.abstract, denote] at hint_n; nomatch hint_n
+      · -- catch-all
+        exact peel_add _ _ typp typq _ hp hq hnone
+    have hx_fv : ∀ v ∈ fv x, («Δ» v).isSome = true :=
+      fun v hv => ht v (by rw [fv, List.mem_append]; left; exact hv)
+    have hy_fv : ∀ v ∈ fv y, («Δ» v).isSome = true :=
+      fun v hv => ht v (by rw [fv, List.mem_append]; right; exact hv)
+    have hsx_fv : ∀ v ∈ fv (simplifier x), («Δ» v).isSome = true :=
+      fun v hv => hx_fv v (fv_simplifier wf_t.1 hv)
+    have hsy_fv : ∀ v ∈ fv (simplifier y), («Δ» v).isSome = true :=
+      fun v hv => hy_fv v (fv_simplifier wf_t.2 hv)
+    unfold simplifier at h
+    rcases key (simplifier x) (simplifier y) (Typing.simplifier typx) (Typing.simplifier typy)
+        _ hsx_fv hsy_fv h with hsx | hsy
+    · have hx : ⟦x.abstract «Δ» hx_fv⟧ᴮ = none := x_ih hx_fv wf_t.1 typx hsx
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind, hx, Option.bind_none]
+    · have hy : ⟦y.abstract «Δ» hy_fv⟧ᴮ = none := y_ih hy_fv wf_t.2 typy hsy
+      rw [Option.eq_none_iff_forall_ne_some]
+      rintro ⟨Z, _, hZ⟩ den_t
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+        Option.bind_eq_some_iff] at den_t
+      obtain ⟨⟨X, τX, hX⟩, den_x, hmatch⟩ := den_t
+      obtain rfl := denote_welltyped_eq
+        ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .int,
+        Typing.of_abstract hx_fv typx⟩ den_x
+      simp_rw [Option.bind_eq_some_iff] at hmatch
+      obtain ⟨⟨Y, τY, hY⟩, den_y, _⟩ := hmatch
+      exact absurd (hy.symm.trans den_y) (by simp)
+  | mul x y x_ih y_ih =>
+    obtain ⟨rfl, typx, typy⟩ := Typing.mulE typ_t
+    -- peel `⟦a *ᴮ b⟧ = none` into a disjunction
+    have peel_mul : ∀ (a b : Term), Γ ⊢ᴮ a : .int → Γ ⊢ᴮ b : .int →
+        ∀ (hab : ∀ v ∈ fv (a *ᴮ b), («Δ» v).isSome = true)
+          (ha : ∀ v ∈ fv a, («Δ» v).isSome = true)
+          (hb : ∀ v ∈ fv b, («Δ» v).isSome = true),
+        ⟦(a *ᴮ b).abstract «Δ» hab⟧ᴮ = none →
+        ⟦a.abstract «Δ» ha⟧ᴮ = none ∨ ⟦b.abstract «Δ» hb⟧ᴮ = none := by
+      intro a b typa typb hab ha hb hnone
+      by_cases ha' : ⟦a.abstract «Δ» ha⟧ᴮ = none
+      · exact Or.inl ha'
+      · obtain ⟨⟨A, _, hA⟩, den_a⟩ := Option.ne_none_iff_exists'.mp ha'
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .int,
+          Typing.of_abstract ha typa⟩ den_a
+        right
+        rw [Option.eq_none_iff_forall_ne_some]
+        rintro ⟨B, _, hB⟩ den_b
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .int,
+          Typing.of_abstract hb typb⟩ den_b
+        simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+          Option.bind_eq_none_iff] at hnone
+        specialize hnone ⟨A, .int, hA⟩ den_a
+        rw [Option.bind_eq_none_iff] at hnone
+        exact absurd (hnone ⟨B, .int, hB⟩ den_b) (by simp)
+    -- `⟦a⟧ = none → ⟦a *ᴮ b⟧ = none`
+    have mul_none_l : ∀ (a b : Term)
+        (hab : ∀ v ∈ fv (a *ᴮ b), («Δ» v).isSome = true)
+        (ha : ∀ v ∈ fv a, («Δ» v).isSome = true),
+        ⟦a.abstract «Δ» ha⟧ᴮ = none → ⟦(a *ᴮ b).abstract «Δ» hab⟧ᴮ = none := by
+      intro a b hab ha hnone
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind, hnone,
+        Option.bind_none]
+    -- self-contained fact about `simplifier_aux_mul`
+    have key : ∀ (p q : Term), Γ ⊢ᴮ p : .int → Γ ⊢ᴮ q : .int →
+        ∀ (hpq : ∀ v ∈ fv (simplifier_aux_mul p q), («Δ» v).isSome = true)
+          (hp : ∀ v ∈ fv p, («Δ» v).isSome = true)
+          (hq : ∀ v ∈ fv q, («Δ» v).isSome = true),
+        ⟦(simplifier_aux_mul p q).abstract «Δ» hpq⟧ᴮ = none →
+        ⟦p.abstract «Δ» hp⟧ᴮ = none ∨ ⟦q.abstract «Δ» hq⟧ᴮ = none := by
+      intro p q typp typq hpq hp hq hnone
+      unfold simplifier_aux_mul at hnone
+      split at hnone
+      · simp_rw [Term.abstract, denote] at hnone; nomatch hnone
+      · simp_rw [Term.abstract, denote] at hnone; nomatch hnone
+      · exact Or.inr hnone
+      · exact Or.inl hnone
+      · simp_rw [Term.abstract, denote] at hnone; nomatch hnone
+      · -- p = x' *ᴮ int a, q = int b ; result x' *ᴮ int (a*b)
+        left
+        obtain ⟨_, typx', _⟩ := Typing.mulE typp
+        refine mul_none_l _ _ hp
+          (fun v hv => hp v (by rw [fv, List.mem_append]; left; exact hv)) ?_
+        rcases peel_mul _ _ typx' Typing.int _
+          (fun v hv => hp v (by rw [fv, List.mem_append]; left; exact hv))
+          (fun v hv => by rw [fv] at hv; nomatch hv) hnone with hx'_n | hint_n
+        · exact hx'_n
+        · simp_rw [Term.abstract, denote] at hint_n; nomatch hint_n
+      · -- catch-all
+        exact peel_mul _ _ typp typq _ hp hq hnone
+    have hx_fv : ∀ v ∈ fv x, («Δ» v).isSome = true :=
+      fun v hv => ht v (by rw [fv, List.mem_append]; left; exact hv)
+    have hy_fv : ∀ v ∈ fv y, («Δ» v).isSome = true :=
+      fun v hv => ht v (by rw [fv, List.mem_append]; right; exact hv)
+    have hsx_fv : ∀ v ∈ fv (simplifier x), («Δ» v).isSome = true :=
+      fun v hv => hx_fv v (fv_simplifier wf_t.1 hv)
+    have hsy_fv : ∀ v ∈ fv (simplifier y), («Δ» v).isSome = true :=
+      fun v hv => hy_fv v (fv_simplifier wf_t.2 hv)
+    unfold simplifier at h
+    rcases key (simplifier x) (simplifier y) (Typing.simplifier typx) (Typing.simplifier typy)
+        _ hsx_fv hsy_fv h with hsx | hsy
+    · have hx : ⟦x.abstract «Δ» hx_fv⟧ᴮ = none := x_ih hx_fv wf_t.1 typx hsx
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind, hx, Option.bind_none]
+    · have hy : ⟦y.abstract «Δ» hy_fv⟧ᴮ = none := y_ih hy_fv wf_t.2 typy hsy
+      rw [Option.eq_none_iff_forall_ne_some]
+      rintro ⟨Z, _, hZ⟩ den_t
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+        Option.bind_eq_some_iff] at den_t
+      obtain ⟨⟨X, τX, hX⟩, den_x, hmatch⟩ := den_t
+      obtain rfl := denote_welltyped_eq
+        ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .int,
+        Typing.of_abstract hx_fv typx⟩ den_x
+      simp_rw [Option.bind_eq_some_iff] at hmatch
+      obtain ⟨⟨Y, τY, hY⟩, den_y, _⟩ := hmatch
+      exact absurd (hy.symm.trans den_y) (by simp)
   | le x y x_ih y_ih =>
     unfold simplifier at h
     obtain ⟨rfl, typx, typy⟩ := Typing.leE typ_t
@@ -203,7 +373,102 @@ theorem simplifier_partial_correct' {t : Term} {«Δ»}
     have hx : ⟦x.abstract «Δ» (fun v hv => ht v (by rw [fv]; exact hv))⟧ᴮ = none :=
       ih (fun v hv => ht v (by rw [fv]; exact hv)) wf_t typx hsimpx
     simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind, hx, Option.bind_none]
-  | eq x y x_ih y_ih => sorry
+  | eq x y x_ih y_ih =>
+    obtain ⟨rfl, α, typx, typy⟩ := Typing.eqE typ_t
+    -- peel `⟦a =ᴮ b⟧ = none` into a disjunction, given both sides typed `α`
+    have peel_eq : ∀ (a b : Term), Γ ⊢ᴮ a : α → Γ ⊢ᴮ b : α →
+        ∀ (hab : ∀ v ∈ fv (a =ᴮ b), («Δ» v).isSome = true)
+          (ha : ∀ v ∈ fv a, («Δ» v).isSome = true)
+          (hb : ∀ v ∈ fv b, («Δ» v).isSome = true),
+        ⟦(a =ᴮ b).abstract «Δ» hab⟧ᴮ = none →
+        ⟦a.abstract «Δ» ha⟧ᴮ = none ∨ ⟦b.abstract «Δ» hb⟧ᴮ = none := by
+      intro a b typa typb hab ha hb hnone
+      by_cases ha' : ⟦a.abstract «Δ» ha⟧ᴮ = none
+      · exact Or.inl ha'
+      · obtain ⟨⟨A, _, hA⟩, den_a⟩ := Option.ne_none_iff_exists'.mp ha'
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, α,
+          Typing.of_abstract ha typa⟩ den_a
+        right
+        rw [Option.eq_none_iff_forall_ne_some]
+        rintro ⟨B, _, hB⟩ den_b
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, α,
+          Typing.of_abstract hb typb⟩ den_b
+        simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+          Option.bind_eq_none_iff] at hnone
+        specialize hnone ⟨A, α, hA⟩ den_a
+        exact absurd (hnone ⟨B, α, hB⟩ den_b) (by simp)
+    -- self-contained fact about `simplifier_aux_eq`
+    have key : ∀ (p q : Term), Γ ⊢ᴮ p : α → Γ ⊢ᴮ q : α →
+        ∀ (hpq : ∀ v ∈ fv (simplifier_aux_eq p q), («Δ» v).isSome = true)
+          (hp : ∀ v ∈ fv p, («Δ» v).isSome = true)
+          (hq : ∀ v ∈ fv q, («Δ» v).isSome = true),
+        ⟦(simplifier_aux_eq p q).abstract «Δ» hpq⟧ᴮ = none →
+        ⟦p.abstract «Δ» hp⟧ᴮ = none ∨ ⟦q.abstract «Δ» hq⟧ᴮ = none := by
+      intro p q typp typq hpq hp hq hnone
+      unfold simplifier_aux_eq at hnone
+      split at hnone
+      · -- var v', var v
+        split_ifs at hnone with hvv
+        · simp_rw [Term.abstract, denote] at hnone; nomatch hnone
+        · exact peel_eq _ _ typp typq _ hp hq hnone
+      · -- e, var v
+        exact (peel_eq _ _ typq typp _ hq hp hnone).symm
+      · -- p, bool true
+        exact Or.inl hnone
+      · -- bool true, q
+        exact Or.inr hnone
+      · -- p, bool false  (q is the literal `bool false`, so α = .bool)
+        cases typq
+        left
+        simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+          Option.bind_eq_none_iff] at hnone
+        rw [Option.eq_none_iff_forall_ne_some]
+        rintro ⟨A, _, hA⟩ den_p
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .bool,
+          Typing.of_abstract hp typp⟩ den_p
+        exact absurd (hnone ⟨A, .bool, hA⟩ den_p) (by simp)
+      · -- bool false, q  (p is the literal `bool false`, so α = .bool)
+        cases typp
+        right
+        simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+          Option.bind_eq_none_iff] at hnone
+        rw [Option.eq_none_iff_forall_ne_some]
+        rintro ⟨A, _, hA⟩ den_q
+        obtain rfl := denote_welltyped_eq
+          ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, .bool,
+          Typing.of_abstract hq typq⟩ den_q
+        exact absurd (hnone ⟨A, .bool, hA⟩ den_q) (by simp)
+      · -- catch-all p, q
+        split_ifs at hnone with hpq'
+        · simp_rw [Term.abstract, denote] at hnone; nomatch hnone
+        · exact peel_eq _ _ typp typq _ hp hq hnone
+    have hx_fv : ∀ v ∈ fv x, («Δ» v).isSome = true :=
+      fun v hv => ht v (by rw [fv, List.mem_append]; left; exact hv)
+    have hy_fv : ∀ v ∈ fv y, («Δ» v).isSome = true :=
+      fun v hv => ht v (by rw [fv, List.mem_append]; right; exact hv)
+    have hsx_fv : ∀ v ∈ fv (simplifier x), («Δ» v).isSome = true :=
+      fun v hv => hx_fv v (fv_simplifier wf_t.1 hv)
+    have hsy_fv : ∀ v ∈ fv (simplifier y), («Δ» v).isSome = true :=
+      fun v hv => hy_fv v (fv_simplifier wf_t.2 hv)
+    unfold simplifier at h
+    rcases key (simplifier x) (simplifier y) (Typing.simplifier typx) (Typing.simplifier typy)
+        _ hsx_fv hsy_fv h with hsx | hsy
+    · have hx : ⟦x.abstract «Δ» hx_fv⟧ᴮ = none := x_ih hx_fv wf_t.1 typx hsx
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind, hx, Option.bind_none]
+    · have hy : ⟦y.abstract «Δ» hy_fv⟧ᴮ = none := y_ih hy_fv wf_t.2 typy hsy
+      rw [Option.eq_none_iff_forall_ne_some]
+      rintro ⟨Z, _, hZ⟩ den_t
+      simp_rw [Term.abstract, denote, Option.pure_def, Option.bind_eq_bind,
+        Option.bind_eq_some_iff] at den_t
+      obtain ⟨⟨X, τX, hX⟩, den_x, hmatch⟩ := den_t
+      obtain rfl := denote_welltyped_eq
+        ⟨Γ.abstract («Δ» := «Δ»), WFTC.of_abstract, α,
+        Typing.of_abstract hx_fv typx⟩ den_x
+      obtain ⟨⟨Y, τY, hY⟩, den_y, _⟩ := hmatch
+      exact absurd (hy.symm.trans den_y) (by simp)
   | mem x S x_ih S_ih => sorry
   | collect vs D P D_ih P_ih => sorry
   | pow S ih =>
